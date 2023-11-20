b29a7a268671f5dcd8cf4d4a86f61d9f05fd77c3
Exargna <fox7games@mail.ru>
2023-11-20 17:20:16 +0600

    Разобрался с логическими операторами

---
+++ HomeW-2/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Логическое И = &&
+        // Логическое ИЛИ = ||
+        //     &&                    ||
+        // true  true = true     true  true = true
+        // false  true = false   true  false = true
+        int a = new Scanner(System.in).nextInt();
+        int b = new Scanner(System.in).nextInt();
+        if (a > 5 && b < 5) {
+            System.out.println("Первая переменная > второй");
+        }
+        if (a < 5 && b > 5) {
+            System.out.println("Вторая переменная > первой");
+        }
+    }
+}
